0de71ce6193898212e44e1fa3004c07fc45878dc
fffun <3326449180@qq.com>
2026-02-05 19:59:59 +0800

    init proto

---
+++ gen.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Generating Go code...
+
+REM 1. 生成 Go 代码
+REM 确保 protoc.exe 在你的环境变量 Path 中
+protoc --proto_path=proto ^
+       --go_out=gen/go --go_opt=paths=source_relative ^
+       --go-grpc_out=gen/go --go-grpc_opt=paths=source_relative ^
+       proto/risk/v1/*.proto
+
+echo Done.
+pause
